31c58d272be17050858dd895abb0e2df075be418
Timazlo <timazloy04444@gmail.com>
2023-06-23 20:39:16 +0300

    add: cookie

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const Notification = () => {
+    const [showNotification, setShowNotification] = useState(true);
+
+    useEffect(() => {
+        const isNotificationClosed = localStorage.getItem('notificationClosed');
+        if (isNotificationClosed) {
+            setShowNotification(false);
+        }
+    }, []);
+
+    const handleCloseNotification = () => {
+        localStorage.setItem('notificationClosed', 'true');
+        setShowNotification(false);
+    };
+
+    return (
+        <>
+            {showNotification && (
+                <div className="notification">
+                    <div className="notification__content">
+                        <p>GitHub: <a href="https://github.com/timazloy/react-headphones" target="_blank">https://github.com/timazloy/react-headphones</a></p>
+                        <button className="notification__button" onClick={handleCloseNotification}>Ok</button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default Notification;
